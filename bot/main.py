c4efec5f3a7bb0453d8deb357662d07cc48963a4
JetDanTe <master.neo16@gmail.com>
2026-01-25 02:49:13 +0200

    Revert "Update db logic"

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,6 @@
 from slack_bot import AuditBot
 
-from shared.services.settings.main import settings
 
 if __name__ == "__main__":
-    bot = AuditBot(settings)
+    bot = AuditBot(debug=False)
     bot.start()
